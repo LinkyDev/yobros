a05f013b346d73c5bfc30f99e00915b34a624353
Yuval Sivan <yuval380sivan@gmail.com>
2018-12-06 23:29:08 +0200

    Added apiRouter and Fixed Static paths

--- web-client/src/server/index.js
+++ web-client/src/server/index.js
@@ -1,19 +1,35 @@
 const express = require('express');
 const path = require('path');
 
+const apiRouter = require('./api');
 
+console.log('Initializing server...');
 const config = {
-    port: process.env["PORT"] || 80,
-    publicPath: process.env["PUBLIC_FOLDER"] || './public'
+    port: process.env['PORT'] || 80,
+    publicPath: process.env['PUBLIC_FOLDER'] || './public'
 }
 
 const app = express();
 
-app.use('/res', express.static(
-    path.resolve(__dirname, config.publicPath)
-));
+[ // Loading static folders
+  // Route,    Folder
+    ['/',      'html'],
+    ['/style', 'css'],
+    ['/js',    'js']
+].forEach((paths) => {
+    const folderPath = path.resolve(
+        __dirname,
+        config.publicPath,
+        paths[1]
+    );
 
+    app.use(paths[0], express.static(folderPath));
+});
 
+// Load api router
+app.use('/api', apiRouter);
+
+// Start server
 app.listen(
     config.port,
     () => {
